dcede060ce4bac308aa71e6096689a4a5fdee616
Mohamed Ehab Mohy <mohamedehab92005@gmail.com>
2025-03-10 16:41:54 +0200

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,117 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+const Database = require('./Database');
+
+const db = new Database();
+
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Create POST API to be able to create a new note 
+
+
+
+app.post('/notes',(req, res) => {
+    const body = req.body;
+    console.log("BODY: ",body);
+    db.addNote(body)
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err=>{
+        res.status(500).send(err);
+    })
+
+});
+
+//Create GET API to retrieve all notes
+
+app.get('/notes', (req, res) => {
+    const {title} =req.query;
+    if (title) {
+        db.getNotesByTitle(title)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err=>{
+             res.status(500).send(err);
+        })
+
+    }else {
+        db.getNotes()
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err=>{
+             res.status(500).send(err);
+         })
+    }     
+
+
+});
+
+app.get('/notes/:id', (req, res) => {
+    const { id } = req.params;
+    db.getNoteById(id)
+    .then(data => {
+        if (!data) {
+            res.status(404).send("Note Id does not exist "+id);
+        } else {
+            res.send(data);
+        }
+       
+    })
+    .catch(err=>{
+        res.status(500).send(err);
+    })
+});
+
+app.put('/notes', (req, res) => {
+    
+    db.updatedNote(req.body)
+    .then(data => {
+        if (!data) {
+            res.status(404).send("Note Id does not exist "+id);
+        } else {
+            res.send(data);
+        }
+       
+    })
+    .catch(err=>{
+        res.status(500).send(err);
+    })
+});
+
+app.delete('/notes/:id', (req, res) => {
+    const { id } = req.params;
+    db.deleteNote(id)
+    .then(data => {
+        if (!data) {
+            res.status(404).send("Note Id does not exist "+id);
+        } else {
+            res.send(data);
+        }
+       
+    })
+    .catch(err=>{
+        res.status(500).send(err);
+    })
+});
+
+//Create PUT API to update a note
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server has started on port ${port}...`);
+    db.connect();
+});
+
+
